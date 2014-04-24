e5e951d6aa9b07071dbb1b852936a4a6fb8c18f5
filiph <filip.hracek@gmail.com>
2014-04-25 00:04:13 +0200

    Normalize the xyz_array, add learning_rate parameter

--- create_som.py
+++ create_som.py
@@ -5,22 +5,28 @@ from star import Star
 
 def create_xyz_array(stars):
     assert(isinstance(stars, list))
+    min_x, min_y, min_z = 0, 0, 0
+    max_x, max_y, max_z = 0, 0, 0
+    for star in stars:
+        if star.X < min_x: min_x = star.X
+        if star.Y < min_y: min_y = star.Y
+        if star.Z < min_z: min_z = star.Z
+        if star.X > max_x: max_x = star.X
+        if star.Y > max_y: max_y = star.Y
+        if star.Z > max_z: max_z = star.Z
     coords = []
     for star in stars:
-        try:
-            assert(isinstance(star, Star))
-        except:
-            print(star)
-            print(star.StarID)
-            print(dir(star))
-            raise
-        coords.append([star.X, star.Y, star.Z])
+        assert(isinstance(star, Star))
+        coords.append([
+            (star.X - min_x) / (max_x - min_x),
+            (star.Y - min_y) / (max_y - min_y),
+            (star.Z - min_z) / (max_z - min_z)])
     return np.array(coords)
 
-def organize(stars, width=1000, height=1000, iters=1000):
+def organize(stars, width=1000, height=1000, iters=1000, learning_rate=0.005):
     assert(isinstance(stars, list))
     np_coords = create_xyz_array(stars)
-    som = SimpleSOMMapper((width, height), iters)
+    som = SimpleSOMMapper((width, height), iters, learning_rate=learning_rate)
     print("Starting to train...")
     som.train(np_coords)
     print("... done.")
