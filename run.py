1cb61aed457b7f0441f071f5123aac8911abe5aa
Filip Hracek <filiph@google.com>
2014-07-01 16:12:39 +0200

    Add run.py for running the whole deal

---
+++ run.py
@@ -0,0 +1,17 @@
+# Creating a SOM of stars autonomously
+
+NUMBER_OF_STARS = 1000
+WIDTH = 141
+HEIGHT = 100
+ITERS = 200
+RATE = 0.0005  # <4 minutes
+
+from import_from_hygxyz_csv import *
+all_stars = import_from_csv()
+stars = all_stars[:NUMBER_OF_STARS]
+
+from create_som import *
+organize(stars, width=WIDTH, height=HEIGHT, iters=ITERS, learning_rate=RATE)
+
+import check_variance
+check_variance.show_map(stars, 141, 100)
