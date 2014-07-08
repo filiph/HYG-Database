f67ba849e1cc520d3c37eafd1044cf823158a761
Filip Hracek <filiph@google.com>
2014-07-08 19:59:06 +0200

    Optimize ToroidSOMMapper (use numpy arrays instead of computing distance one by one)

--- toroidsom.py
+++ toroidsom.py
@@ -1,10 +1,82 @@
-
-from mvpa2.suite import SimpleSOMMapper, np, accepts_dataset_as_samples
+import numpy as np
+from mvpa2.suite import SimpleSOMMapper, accepts_dataset_as_samples
 
 if __debug__:
     from mvpa2.base import debug
 
 class ToroidSOMMapper(SimpleSOMMapper):
+    def unfold_influence_kernel(self, bmu, distances_quadrant):
+        infl = np.vstack((
+            np.hstack((
+                # upper left
+                distances_quadrant[bmu[0]:0:-1, bmu[1]:0:-1],
+                # upper right
+                distances_quadrant[bmu[0]:0:-1, :self.kshape[1] - bmu[1]])),
+            np.hstack((
+                # lower left
+                distances_quadrant[:self.kshape[0] - bmu[0], bmu[1]:0:-1],
+                # lower right
+                distances_quadrant[:self.kshape[0] - bmu[0], :self.kshape[1] - bmu[1]]))
+        ))
+        # create same thing, but for "wrapped around" distance of the toroid
+        # diagonal wrap
+        wrapped_diagonal_infl = np.vstack((
+            np.hstack((
+                # upper left
+                distances_quadrant[self.kshape[0] - bmu[0]:, self.kshape[1] - bmu[1]:],
+                # cross top
+                np.zeros((bmu[0], 1)),
+                # upper right
+                distances_quadrant[self.kshape[0] - bmu[0]:, :bmu[1]:-1])),
+            # cross horizontal
+            np.zeros((1, self.kshape[1])),
+            np.hstack((
+                # lower left
+                distances_quadrant[self.kshape[0]:bmu[0]:-1, self.kshape[1]-bmu[1]:],
+                # cross bottom
+                np.zeros((self.kshape[0] - bmu[0] - 1, 1)),
+                # lower right
+                distances_quadrant[self.kshape[0]:bmu[0]:-1, :bmu[1]:-1]))
+        ))
+        np.maximum(infl, wrapped_diagonal_infl, out=infl)
+        # coming from top and bottom
+        top_bottom_infl = np.vstack((
+            np.hstack((
+                # upper left
+                distances_quadrant[self.kshape[0] - bmu[0]:, bmu[1]:0:-1],
+                # upper right
+                distances_quadrant[self.kshape[0] - bmu[0]:, :self.kshape[1] - bmu[1]])),
+            np.zeros((1, self.kshape[1])),
+            np.hstack((
+                # lower left
+                distances_quadrant[:bmu[0]:-1, bmu[1]:0:-1],
+                # lower right
+                distances_quadrant[:bmu[0]:-1, :self.kshape[1] - bmu[1]]
+            ))
+        ))
+        np.maximum(infl, top_bottom_infl, out=infl)
+        # coming from left and right
+        left_right_infl = np.vstack((
+            np.hstack((
+                # upper left
+                distances_quadrant[bmu[0]:0:-1, self.kshape[1] - bmu[1]:],
+                # cross top
+                np.zeros((bmu[0], 1)),
+                # upper right
+                distances_quadrant[bmu[0]:0:-1, :bmu[1]:-1]
+            )),
+            np.hstack((
+                # lower left
+                distances_quadrant[:self.kshape[0] - bmu[0], self.kshape[1] - bmu[1]:],
+                # cross bottom
+                np.zeros((self.kshape[0] - bmu[0], 1)),
+                # lower right
+                distances_quadrant[:self.kshape[0] - bmu[0], :bmu[1]:-1]
+            ))
+        ))
+        np.maximum(infl, left_right_infl, out=infl)
+        return infl
+
     @accepts_dataset_as_samples
     def _train(self, samples):
         """Perform network training.
@@ -20,34 +92,31 @@ class ToroidSOMMapper(SimpleSOMMapper):
         was called with the same argument.
         """
 
+        # ensure that dqd was set properly
+        dqd = self._dqd
+        if dqd is None:
+            raise ValueError("This should not happen - was _pretrain called?")
+
         # units weight vector deltas for batch training
         # (height x width x #features)
         unit_deltas = np.zeros(self._K.shape, dtype='float')
 
-        # a dictionary to store distance matrices for each (row, column) tuple
-        distance_kernels = {}
-
         # for all iterations
         for it in xrange(1, self.niter + 1):
+            # compute the neighborhood impact kernel for this iteration
+            # has to be recomputed since kernel shrinks over time
+            k = self._compute_influence_kernel(it, dqd)
+
             # for all training vectors
             for s in samples:
                 # determine closest unit (as element coordinate)
                 b = self._get_bmu(s)
-
-                # get or create distance kernel
-                if b not in distance_kernels:
-                    distance_kernels[b] = ToroidSOMMapper._get_toroid_distance_kernel(
-                        self.kshape,
-                        b
-                    )
-                dinstance_k = distance_kernels[b]
-
-                # compute the neighborhood impact kernel for this iteration and bmu
-                # has to be recomputed since kernel shrinks over time
-                infl = self._compute_influence_kernel(it, dinstance_k)
-
-                # train all units by  simply multiplying it to the difference of target
+                # train all units at once by unfolding the kernel (from the
+                # single quadrant that is precomputed), cutting it to the
+                # right shape and simply multiply it to the difference of target
                 # and all unit weights....
+                infl = self.unfold_influence_kernel(b, k)
+
                 unit_deltas += infl[:, :, np.newaxis] * (s - self._K)
 
             # apply cumulative unit deltas
@@ -76,4 +145,12 @@ class ToroidSOMMapper(SimpleSOMMapper):
 
 
 if __name__ == "__main__":
-    print(ToroidSOMMapper._get_toroid_distance_kernel((10, 5), (5, 0)))
+    width = 60
+    height = 40
+    iters = 20
+    mapper = ToroidSOMMapper((width, height), iters)
+    distance_metric = lambda x, y: (x ** 2 + y ** 2) ** 0.5
+    dqd = np.fromfunction(distance_metric,
+                          mapper.kshape, dtype='float')
+    k = np.exp((-1.0 * dqd))
+    print(mapper.unfold_influence_kernel((5, 3), k))
